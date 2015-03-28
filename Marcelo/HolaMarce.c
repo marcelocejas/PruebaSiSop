092cf6481931508d3fe1a541b87e9052c06b7c21
marcelocejas <cejas.marcelo@gmail.com>
2015-03-28 20:42:41 -0300

    Cree mi carpeta de codigo

---
+++ Marcelo/HolaMarce.c
@@ -0,0 +1,13 @@
+/*
+ * HolaMarce.c
+ *
+ *  Created on: 28/3/2015
+ *      Author: Marcelo Cejas
+ */
+
+#include<stdio.h>
+int main(int argc, char **argv) {
+	printf("Este es mi lugar !!!");
+	return 0;
+}
+
